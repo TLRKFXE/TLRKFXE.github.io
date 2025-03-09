739afcabc9cd7ff0793cc2189d62af8fa0ca103b
TLRKFXE <1309164405@qq.com>
2025-03-09 09:46:23 +0800

    增加背景

--- script/js.js
+++ script/js.js
@@ -1,7 +1,7 @@
 // 配置对象
 const config = {
     folderPath: 'img/background/',  // 图片目录路径
-    imageNames: ['1.jpg', '2.jpg', '3.jpg'], // 图片文件名
+    imageNames: ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg', '21.jpg', '22.jpg', '23.jpg', '24.jpg', '25.jpg', '26.jpg'], // 图片文件名
     interval: 5000,           // 切换间隔（毫秒）
     fadeDuration: 1000        // 过渡时间（毫秒）
 };
