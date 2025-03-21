5e737782f4bdaf30221ced10cc4c66e5c1477b39
TLRKFXE <1309164405@qq.com>
2025-03-21 22:24:45 +0800

    1

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'tlrkfxegithubio',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,8 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'tlrkfxegithubio',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
+
